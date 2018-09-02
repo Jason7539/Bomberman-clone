af46d00bd094a98657772fea9f8a59f039c42a67
Jason Nguyen <jnguyen7539@gmail.com>
2018-09-01 20:20:44 -0700

    Add files via upload

--- bombs.py
+++ bombs.py
@@ -1,27 +1,26 @@
 # bombs have a damage radius
 # pickups:
-# increase amount of bombs you can place 
-# increase the radius of bombs 
-# bombs can only take out one block at a time 
+# increase amount of bombs you can place
+# increase the radius of bombs
+# bombs can only take out one block at a time
 
 class Bomb():
     def __init__(self):
-        self.raduis = 1 
-    
+        self.raduis = 1
+
     def add_effects(effect):
         """takes a  Pickup object to modify the bomb"""
         name = effect.get_name()
-        # if elif else         
+        # if elif else
         pass
 
 
 class PickUp():
-    
-    def __init__(self, name = "Default"):
+
+    def __init__(self, name="Default"):
         self.name = name
-    
-    def get_name():
+
+    def get_name(self):
         return self.name
 
-
-# make different typ of pickups 
+# make different typ of pickups
