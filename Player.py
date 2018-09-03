70d1737fb0d50515c7b1857ba4a9076a30a4becc
Jason Nguyen <jnguyen7539@gmail.com>
2018-09-02 19:57:41 -0700

    Add files via upload

--- Player.py
+++ Player.py
@@ -2,14 +2,16 @@ import pygame, sys, os, time, util
 from pygame.event import clear
 from pygame.locals import *
 import bombs
+
+
 # to do list
-# need to update image to choose from a list to creat animation 
-# you can place one bomb every x amount of seconds 
+# need to update image to choose from a list to creat animation
+# you can place one bomb every x amount of seconds
 
 class Player(pygame.sprite.Sprite):
     """ Parent class that User controls """
-    
-    def __init__(self, speed = None):
+
+    def __init__(self, speed=None):
         pygame.sprite.Sprite.__init__(self)  # initiate the sprite class
         bomb = bombs.Bomb()
 
@@ -17,36 +19,37 @@ class Player(pygame.sprite.Sprite):
         bombs.add_effects(pickups)
 
 
-
 class PlayerOne(Player):
     """ Actual object that Player one interacts with   """
 
-    # change this for animation 
+    # change this for animation
     def __init__(self):
         """ Constructor """
-        
         Player.__init__(self)
         self.speed = 10
-        
+
         self.image, self.rect = util.load_image(os.path.join('images', 'player-one.png'))
+        self.rect.x = 70
+        self.rect.y = 70
+
+    def update(self, location=None):
+
+        " Move the player position based on the the speed variable and the location argument"
+        if location is "up":
+            self.rect.y -= self.speed
+        elif location is "down":
+            self.rect.y += self.speed
+        elif location is "right":
+            self.rect.x += self.speed
+        elif location is "left":
+            self.rect.x -= self.speed
 
 
-    def update(self, location = None):
-        if location = "up":
-            pass
-        elif location = "right":
-            pass
-        
-        
 
-        self.rect.y += 7
-        
- 
+
 
 class PlayerTwo(Player):
     def __init__(self):
-
         Player.__init__(self)
         self.image, self.rect = util.load_image(os.path.join('images', 'player-two.png'))
         self.speed = 10
-        
--- bomberman.py
+++ bomberman.py
@@ -16,42 +16,45 @@ def main():
 
     # clock object used to control fps 
     clock = pygame.time.Clock()
-    
+
     # tuple representing the x and y value of the window
-    # x = 1050, y = 910 
+    # x = 1050, y = 910
     screen_size = (15 * 70, 13 * 70)
 
-    # create a window where the background will be drawn with the same size of the background 
+    # create a window where the background will be drawn with the same size of the background
     screen = pygame.display.set_mode(screen_size, RESIZABLE)
 
     # upload the background
-    background = pygame.image.load(os.path.join("images", "background.png")) 
+    background = pygame.image.load(os.path.join("images", "background.png"))
 
-    # load the hard blocks images 
+    #new background to clear
+    new_background = pygame.image.load(os.path.join("images", "background.png"))
+    # new_background = pygame.Surface((1050, 910))
+    # new_background.fill((Color("gold")))
+
+    # load the hard blocks images
     hard_blocks = pygame.image.load(os.path.join("images", "hard-block.png"))
 
-    # turn this into a method 
+    # turn this into a method
     # draw the hard blocks on the background
-    
-    # draw to top borders 
-    top_x = 0
-    for i in range(0,14):
-        background.blit(hard_blocks, (top_x,0))
-        top_x += 70
 
+    # # draw to top borders
+    top_x = 0
+    for i in range(0, 14):
+        background.blit(hard_blocks, (top_x, 0))
+        top_x += 70
 
     # draw the right border
     right_y = 0
     for i in range(0, 13):
         background.blit(hard_blocks, (top_x, right_y))
         right_y += 70
-        
+
     # draw the left border
     left_y = 0
     for i in range(0, 13):
         background.blit(hard_blocks, (0, left_y))
         left_y += 70
-
 
     # draw the bottom border
     left_y -= 70
@@ -59,33 +62,64 @@ def main():
     for i in range(0, 14):
         background.blit(hard_blocks, (bottom_x, left_y))
         bottom_x += 70
-        
 
-    
-    # draw background on the screen 
-    screen.blit(background, (0,0))
+    # draw background on the screen
+    screen.blit(background, (0, 0))
 
-    
-    #create game objects
+    # create game objects
     p1 = Player.PlayerOne()
     one_group = pygame.sprite.Group(p1)
-    
-    one_group.draw(screen)
-    #main loop
+
+    # variable that updates player movement
+    location = ""
+
+    # main loop
     while 1:
-        #makes the game run at 60 fps 
+        # makes the game run at 60 fps
         clock.tick(60)
         for event in pygame.event.get():
-            # quit the game and exit the program if the close button is clicked 
+            # quit the game and exit the program if the close button is clicked
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
-        #one_group.update()
+            # read and respond when a key is pressed down
+            elif event.type == KEYDOWN:
+                # pause the game
+                if event.key == K_ESCAPE:
+                    pygame.time.delay(1000)
+
+                # control player movement
+                if event.key == K_w:
+                    location = "up"
+                elif event.key == K_s:
+                    location = "down"
+                elif event.key == K_a:
+                    location = "left"
+                elif event.key == K_d:
+                    location = "right"
+
+            elif event.type == KEYUP:
+                # process controls player movement when a key is let go
+                if event.key == K_w:
+                    location = ""
+                elif event.key == K_s:
+                    location = ""
+                elif event.key == K_a:
+                    location = ""
+                elif event.key == K_d:
+                    location = ""
+
+
+
+        one_group.update(location)
+        one_group.clear(screen, new_background)
         one_group.draw(screen)
-        
+
+
         pygame.display.update()
+
+        pygame.event.pump() # clear the event queue
 
 
 main()
-
